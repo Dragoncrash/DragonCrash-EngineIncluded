081f316be579d10330a4709e0b5862f10deda66c
Edmond T Dooley <etdooley@crm.ucsc.edu>
2017-04-19 14:36:29 -0700

    Added new properly made project which should compile correctly.

---
+++ UnrealEngine-4.14/DragonCrashNew/Source/DragonCrashNew/Private/KeyRemappings.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DragonCrashNew.h"
+#include "KeyRemappings.h"
+
+//FText UCreateNewObject::HelloWorld()
+//{
+//	return FText::FromString("Hello World");
+//}

ad61c150a41fc33ccf1781bc379f21b4e5080fbd
Norma Tu <NormaTu545@gmail.com>
2017-04-24 15:47:25 -0700

    Everything for main menu is here. Just needs to be merged by  hand. Don't know where the extra projects came from.

---
+++ UnrealEngine-4.14/DragonCrashNew 4.14/Source/DragonCrashNew/DragonCrashNew.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DragonCrashNew.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragonCrashNew, "DragonCrashNew" );
